2045cd68113769ee430332f99db443e491e79c02
Simon Krenger <simon@krenger.ch>
2012-01-17 18:38:39 +0100

    Applied bug fix to mu-code.c, added some comments
    - Header added
    - Bugfix to correct char array
    - Simplified addition of address with indexed addressing modes

--- c/alu4students/src/cpu/mu-code.c
+++ c/alu4students/src/cpu/mu-code.c
@@ -1,8 +1,15 @@
+/*
+ * mu-code.c
+ *
+ * Last change: Jan 17, 2012
+ *      Author: Simon Krenger <krens1@bfh.ch>
+ */
 #include <stdio.h>
 #include <string.h>
 
 #include "mu-code.h"
 #include "cpu.h"
+#include "alu.h"
 #include "register.h"
 
 struct cpu6502 {
@@ -26,7 +33,7 @@ struct cpu6502 {
 	// Index register
 	char *ir;
 
-	// Databus register
+	// Data bus register
 	char *dbr;
 
 	// Adr. bus
@@ -65,18 +72,21 @@ void cpu_6502_lda_imm(struct cpu6502 *cpu) {
 }
 
 /**
- * A full 16-bit address is specified and the byte at that address is used to perform the computation.
+ * Absolute addressing
+ *
+ * A full 16-bit address is specified and the byte at that address is used to
+ * perform the computation.
  */
 void cpu_6502_lda_absolute(struct cpu6502 *cpu) {
 
-	// Get low byte
+	// Get low byte (Current PC)
 	cp_register(cpu->pcl, cpu->abrl);
 	cp_register(cpu->pch, cpu->abrh);
 
 	set_rw2read();
 	access_memory();
 
-	char low_byte;
+	char low_byte[REG_WIDTH + 1];
 	cp_register(cpu->dbr, &low_byte);
 
 	// Get high byte
@@ -87,16 +97,17 @@ void cpu_6502_lda_absolute(struct cpu6502 *cpu) {
 	set_rw2read();
 	access_memory();
 
-	// Set the address
+	// Set the address to what we just read
 	cp_register(cpu->dbr, cpu->abrh);
 	cp_register(&low_byte, cpu->abrl);
 
 	set_rw2read();
 	access_memory();
 
-	// Actually get the byte
+	// Retrieve the byte
 	cp_register(cpu->dbr, cpu->acc);
 
+	// Set flags
 	if (cpu->acc == '1')
 		setSignflag(cpu->flags);
 	else
@@ -111,10 +122,13 @@ void cpu_6502_lda_absolute(struct cpu6502 *cpu) {
 		}
 	}
 
+	// Next instruction
 	inc_pc();
 }
 
 /**
+ * Absolute addressing with index
+ *
  * The address to be accessed by an instruction using Y register indexed
  * absolute addressing is computed by taking the 16 bit address from the
  * instruction and added the contents of the Y register.
@@ -128,7 +142,7 @@ void cpu_6502_lda_absolute_y(struct cpu6502 *cpu) {
 	set_rw2read();
 	access_memory();
 
-	char low_byte;
+	char low_byte[REG_WIDTH + 1];
 	cp_register(cpu->dbr, &low_byte);
 
 	// Get high byte
@@ -139,15 +153,16 @@ void cpu_6502_lda_absolute_y(struct cpu6502 *cpu) {
 	set_rw2read();
 	access_memory();
 
-	char high_byte;
+	char high_byte[REG_WIDTH + 1];
 	cp_register(cpu->dbr, &high_byte);
 
-	// Add Y to address
-	op_addc(cpu->idy, low_byte, cpu->acc, cpu->flags);
+	// Add Y to address (low byte first)
+	op_add(cpu->idy, low_byte, cpu->acc, cpu->flags);
 	cp_register(cpu->acc, low_byte);
-	if (getCarryflag(cpu->flags) == '1') {
-		op_add("00000001", high_byte, high_byte, cpu->flags);
-	}
+
+	// Add Y to address (high byte)
+	op_addc("00000000", high_byte, cpu->acc, cpu->flags);
+	cp_register(cpu->acc, high_byte);
 
 	// Write the now modified address to address bus
 	cp_register(high_byte, cpu->abrh);
@@ -173,10 +188,13 @@ void cpu_6502_lda_absolute_y(struct cpu6502 *cpu) {
 		}
 	}
 
+	// Next instruction
 	inc_pc();
 }
 
 /**
+ * Indexed indirect addressing
+ *
  * Indexed indirect addressing is normally used in conjunction with a table of
  * address held on zero page. The address of the table is taken from the
  * instruction and the X register added to it (with zero page wrap around) to
@@ -191,19 +209,20 @@ void cpu_6502_lda_index_ind(struct cpu6502 *cpu) {
 	set_rw2read();
 	access_memory();
 
-	char low_byte;
+	char low_byte[REG_WIDTH + 1];
 	cp_register(cpu->dbr, &low_byte);
 
 	// Add X to address
-	op_addc(cpu->idx, low_byte, cpu->acc, cpu->flags);
+	op_add(cpu->idx, low_byte, cpu->acc, cpu->flags);
 	cp_register(cpu->acc, low_byte);
 
 	char high_byte[REG_WIDTH + 1] = "00000000";
-	if (getCarryflag(cpu->flags) == '1') {
-		op_add("00000001", high_byte, high_byte, cpu->flags);
-	}
+	op_addc("00000000", high_byte, cpu->acc, cpu->flags);
+	cp_register(cpu->acc, low_byte);
 
-	// Get low byte
+	// At this point, we have the address of the address
+
+	// Get low byte of address
 	cp_register(low_byte, cpu->abrl);
 	cp_register(high_byte, cpu->abrh);
 
@@ -212,7 +231,7 @@ void cpu_6502_lda_index_ind(struct cpu6502 *cpu) {
 
 	cp_register(cpu->dbr, low_byte);
 
-	// Get high byte
+	// Get high byte of address
 	inc_pc();
 	cp_register(cpu->pcl, cpu->abrl);
 	cp_register(cpu->pch, cpu->abrh);
@@ -220,7 +239,7 @@ void cpu_6502_lda_index_ind(struct cpu6502 *cpu) {
 	set_rw2read();
 	access_memory();
 
-	// Now get the actual value
+	// Now we have what we need, get the actual value
 	cp_register(cpu->dbr, cpu->abrh);
 	cp_register(low_byte, cpu->abrl);
 
@@ -244,5 +263,6 @@ void cpu_6502_lda_index_ind(struct cpu6502 *cpu) {
 		}
 	}
 
+	// Next instruction
 	inc_pc();
 }
